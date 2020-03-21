9d6ff5d3c05b1d6d526437a546dcb3e902275c4d
madealexaguiar <alex.aguiar@madeinweb.com.br>
2020-03-21 20:31:50 -0300

    first commit

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1584832983,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/platform-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/silver_master.m4a",
+		"media/boss.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
